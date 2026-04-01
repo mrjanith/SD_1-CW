c06a90fb0fa3b94c1a708d9623163f691b8f51e9
Janith Bodaragama <janithofficial@gmial.com>
2026-04-01 16:04:43 +0530

    Commit initial files

---
+++ Digital_Recipe_Book_Manager.py
@@ -0,0 +1,13 @@
+recipes = {
+    "RCP001" : {
+        "name" : "Pasta",
+        "ingredients" : [("Pasta", 200, "g")],
+        "time" : "00:30",
+        "category" : "Breakfast",
+        "tags" : {"Italian", "Vegetarian"}
+
+    }
+
+}
+
+
